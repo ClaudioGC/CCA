410f01e5e0c1c6c320cfbb02190a5d417a69b89f
ClaudioGC <gomezcorralclaudio@gmail.com>
2015-07-19 22:57:34 -0400

    function

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	
+	$('h2').mouseenter(function(){
+		$('h2').fadeTo('fast', 1);
+	});
+
+	$('h2').mouseleave(function(){
+		$('h2').fadeTo('fast', 0.5);
+	});
+
+});
